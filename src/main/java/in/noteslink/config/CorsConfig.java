92f7b4118552139758cc46a240030f81c5cb3f80
Rohit Kumar Chaurasiya <rohitkumarchaurasiya111@gmail.com>
2026-08-29 20:42:01 +0530

    Adding allowed origins in .env

--- src/main/java/in/noteslink/config/CorsConfig.java
+++ src/main/java/in/noteslink/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package in.noteslink.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,13 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
+// Storing the allowed origins in .env
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
